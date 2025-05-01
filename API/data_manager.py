9a4125a66f8aeba6a4170864546a129d3286d498
DMJamboe <sjevans400@gmail.com>
2025-05-01 14:07:47 +0000

    Make log file now append monthly.

--- API/data_manager.py
+++ API/data_manager.py
@@ -2,7 +2,6 @@ import json
 import os
 from models import PintEconomy
 import datetime
-from uuid import uuid4 as uuid
 
 DATA_FILE = os.environ.get("DATA_FILE","PintEconomy.json")
 LOG_PATH = os.environ.get("LOG_PATH", "logs")
@@ -25,12 +24,14 @@ def save_data(data: PintEconomy):
 
 def save_transaction(transaction: str, pint_quantity: str, debtor_id: str, creditor_id: str, reason: str):
     now = datetime.datetime.now().isoformat()
-    filename: str = os.path.join(LOG_PATH, f"{now}-{uuid().hex}.txt")
-    with open(filename, "w") as logfile:
-        logfile.writelines([
-            f"type={transaction}\n",
-            f"amount={pint_quantity}\n",
-            f"debtor={debtor_id}\n",
-            f"creditor={creditor_id}\n",
-            f"reason={reason}\n"
-        ])
+    filename: str = os.path.join(LOG_PATH, f"logs-{datetime.datetime.now().year}-{datetime.datetime.now().month}.csv")
+
+    with open(filename, "a") as logfile:
+        logfile.write(','.join([
+            now,
+            transaction,
+            pint_quantity,
+            debtor_id,
+            creditor_id,
+            reason
+        ]))
