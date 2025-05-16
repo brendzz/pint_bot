02cfae6fcd2338d8e11ed607c01926922cf9f850
t-spar <tom@sparham.com>
2025-05-16 17:43:24 +0100

    Added method for returning categorised commands

--- bot/command.py
+++ bot/command.py
@@ -1,4 +1,5 @@
 """Module for defining the command dataclass."""
+from collections import defaultdict
 from dataclasses import dataclass
 from typing import ClassVar
 
@@ -38,3 +39,11 @@ class Command:
         """Returns a list of all registered commands in the order they were added."""
         # returns in insertion order
         return list(cls._registry.values())
+    
+    @classmethod
+    def all_by_category(cls) -> dict[str, list["Command"]]:
+        """Returns all commands grouped by their category."""
+        groups = defaultdict(list)
+        for command in cls._registry.values():
+            groups[command.category].append(command)
+        return dict(groups)
