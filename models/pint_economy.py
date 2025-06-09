68371909a1a8f6db8c4311991b0eafbc6ae8e44c
t-spar <tom@sparham.com>
2025-06-10 00:54:12 +0100

    Deleted pint economy model

--- models/pint_economy.py
+++
@@ -1,8 +0,0 @@
-from pydantic import BaseModel, Field
-from typing import Dict
-
-from models.user_data import UserData
-
-class PintEconomy(BaseModel):
-    """Main data structure for the debt management system."""
-    users: Dict[str, UserData] = Field(default_factory=dict)
