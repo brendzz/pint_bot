02841ac52471ff495b85fa48ad0c1b9767c2ca7f
t-spar <tom@sparham.com>
2025-06-10 00:55:05 +0100

    Updated init file for new models

--- models/__init__.py
+++ models/__init__.py
@@ -1,21 +1,25 @@
 from .debt_entry import DebtEntry
+from .debts_data import DebtsData
 from .owe_request import OweRequest
-from .pint_economy import PintEconomy
 from .set_unicode_preference_request import SetUnicodePreferenceRequest
 from .settle_request import SettleRequest
 from .user_debts import UserDebts
 from .user_data import UserData
 from .user_preferences import UserPreferences
 from .transaction_entry import TransactionEntry
+from .transactions_data import TransactionsData
+from .preferences_data import PreferencesData
 
 __all__ = [
     "DebtEntry",
+    "DebtsData",
     "OweRequest",
-    "PintEconomy",
     "SetUnicodePreferenceRequest",
     "SettleRequest",
     "UserDebts",
     "UserData",
     "UserPreferences",
-    "TransactionEntry"
+    "TransactionEntry",
+    "TransactionsData",
+    "PreferencesData"
 ]
