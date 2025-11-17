4e31646f3bc61b28e3f1566b56623127a929ba78
brendzz <brendan_head@yahoo.co.uk>
2025-11-17 21:09:35 +0000

    Minor fixes

--- bot/utilities/message_processor.py
+++ bot/utilities/message_processor.py
@@ -6,39 +6,47 @@ async def send_message(
     interaction: discord.Interaction,
     title: str,
     description: str,
-    color: discord.Color
+    color: discord.Color,
+    ephermal: bool = False
 ):
     """Sends an embed message, splitting into multiple messages if the description is too long.
     Splits at newlines only if needed."""
-    await check_title_length(interaction, title)
+    await check_title_length(interaction, title, ephermal)
 
     # If description fits, send directly without splitting
     if len(description) <= config.DISCORD_EMBED_DESCRIPTION_LIMIT:
         embed = discord.Embed(title=title, description=description, color=color)
-        await interaction.followup.send(embed=embed)
-    else:
-        await split_message_and_send(interaction,title,description,color)
 
-async def check_title_length(interaction: discord.Interaction, title: str):
+        if not interaction.response.is_done():
+            await interaction.response.send_message(embed=embed, ephemeral=ephermal)
+        else:
+            await interaction.followup.send(embed=embed, ephemeral=ephermal)
+    else:
+        await split_message_and_send(interaction,title,description,color,ephermal)
+
+async def check_title_length(interaction: discord.Interaction, title: str, ephermal:bool):
     """Checks the title matches discord character limits"""
     if len(title) > config.DISCORD_EMBED_TITLE_LIMIT:
-            await send_message(
-                interaction,
-                "Formatting Error",
-                f"Title exceeds Discord's {config.DISCORD_EMBED_TITLE_LIMIT} character limit. Sorry about that",
-                discord.Color.red()
-            )
+        await send_message(
+            interaction,
+            "", #this is blank to prevent infinite loop if DISCORD_EMBED_TITLE_LIMIT is set to a low number
+            f"Title exceeds Discord's {config.DISCORD_EMBED_TITLE_LIMIT} character limit. Sorry about that",
+            discord.Color.red(),
+            ephermal
+        )
+
 
 async def split_message_and_send(
     interaction: discord.Interaction,
     title: str,
     description: str,
     color: discord.Color,
+    ephermal: bool
 ):
     """Sends a split message across multiple embeds"""
     chunks = split_text_into_chunks(description, config.DISCORD_EMBED_DESCRIPTION_LIMIT)
 
-    await send_embed_chunks(interaction, title, chunks, color)
+    await send_embed_chunks(interaction, title, chunks, color, ephermal)
 
 
 def split_text_into_chunks(text: str, limit: int) -> list[str]:
@@ -84,7 +92,8 @@ async def send_embed_chunks(
     title: str,
     chunks: list[str],
     color: discord.Color,
-):
+    ephermal: bool = False
+    ):
     """Sends the embed messages one by one, including the title only in the first."""
     for i, chunk in enumerate(chunks):
         embed = discord.Embed(
@@ -92,4 +101,7 @@ async def send_embed_chunks(
             description=chunk,
             color=color
         )
-        await interaction.followup.send(embed=embed)
+        if not interaction.response.is_done():
+            await interaction.response.send_message(embed=embed, ephemeral=ephermal)
+        else:
+            await interaction.followup.send(embed=embed, ephemeral=ephermal)
